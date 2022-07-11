be219165e2ffb2e70aa1bb3bba935d9620af6e79
yunjs24 <yunjs24@gmail.com>
2022-07-11 22:31:00 +0900

    Add ContentLogo in leftside of section

--- src/components/LandingPage/Secsions/StatisticSection/ContentLogo/ContentLogo.js
+++ src/components/LandingPage/Secsions/StatisticSection/ContentLogo/ContentLogo.js
@@ -1,30 +1,28 @@
 import styled from 'styled-components'
 
+import { fadeinAnimation } from '../../../../../utils/fadein'
 import Logo from '../images/triple2x.png'
 
 function ContentLogo() {
   return (
-    <StyledLogo>
-      <p>2021년 12월 기준</p>
-    </StyledLogo>
+    <div style={{ width: '400px' }}>
+      <StyledLogo>
+        <p>2021년 12월 기준</p>
+      </StyledLogo>
+    </div>
   )
 }
+
 const StyledLogo = styled.article`
   font-size: 15px;
-  text-align: center;
-
-  position: absolute;
-
-  width: 400px;
-  height: 338px;
-
+  min-width: 400px;
   padding-top: 280px;
 
   box-sizing: border-box;
-
   background-image: url(${Logo});
   background-size: 400px 338px;
   background-repeat: no-repeat;
+  ${fadeinAnimation}
 `
 
 export default ContentLogo
